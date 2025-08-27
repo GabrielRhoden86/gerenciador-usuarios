a6cfb49fefc8a51b8d15a9871a67e7a365e986c2
gabriel_rhoden <gabriel_rhoden@econeteditora.com.br>
2025-08-27 09:04:48 -0300

    ajuste entry point

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,18 +1,22 @@
 #!/bin/sh
 
-# Instala as dependências do Composer.
+# Instala as dependências do Composer
 composer install --no-dev --optimize-autoloader
 
-# Concede permissões de escrita para as pastas de armazenamento e cache.
-chmod -R 775 /var/www/html/storage
-chmod -R 775 /var/www/html/bootstrap/cache
+# Concede permissões para as pastas do Laravel
+# Usamos 'chown' para garantir que as permissões de usuário estejam corretas
+chown -R www-data:www-data /var/www/html/storage
+chown -R www-data:www-data /var/www/html/bootstrap/cache
 
-# Executa as migrações do banco de dados.
+# Executa as migrações do banco de dados
 php artisan migrate --force
 
-# Limpa o cache.
+# Limpa o cache
 php artisan cache:clear
 php artisan optimize:clear
 
-# Inicia o servidor do Laravel usando a variável de ambiente PORT do Railway.
-php artisan serve --host=0.0.0.0 --port=${PORT}
+# Inicia o PHP-FPM em primeiro plano
+/usr/sbin/php-fpm8.2 -F
+
+# Inicia o Nginx em primeiro plano
+nginx -g "daemon off;"
