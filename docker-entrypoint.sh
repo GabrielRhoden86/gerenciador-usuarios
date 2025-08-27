541843cf7be1792992d0fbfb70c0916ac93b6a7e
Gabriel Rhoden <gabrielrhodden@gmail.com>
2025-08-26 23:27:29 -0300

    correçÃO

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,22 +1,13 @@
 #!/bin/sh
 
-if [ -z "$APP_KEY" ]; then
-    echo "A variável de ambiente APP_KEY não está definida."
-    exit 1
-fi
-
-echo "Concedendo permissões..."
 chmod -R 775 /var/www/html/storage
 chmod -R 775 /var/www/html/bootstrap/cache
 
-echo "Instalando dependências do Composer..."
 composer install --no-dev --optimize-autoloader
 
-echo "Limpando o cache da aplicação..."
-php artisan optimize:clear
-
-echo "Executando migrações do banco de dados..."
 php artisan migrate --force
 
-echo "Iniciando o servidor..."
+php artisan cache:clear
+php artisan optimize:clear
+
 php artisan serve --host=0.0.0.0 --port=8000
