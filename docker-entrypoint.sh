22a18fd792211b8568ec7de48a62d67caa3800ae
Gabriel Rhoden <gabrielrhodden@gmail.com>
2025-08-27 20:55:08 -0300

    ajuste docker

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -15,7 +15,6 @@ php artisan cache:clear
 php artisan config:clear
 php artisan route:clear
 php artisan view:clear
-php artisan db:seed --class=DatabaseSeeder
 php artisan optimize:clear
 
 echo "🚀 Iniciando Laravel na porta \$PORT..."
