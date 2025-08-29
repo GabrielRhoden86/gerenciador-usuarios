437ef4e72bdd6a3018b3700046552aa1185e8461
gabriel_rhoden <gabriel_rhoden@econeteditora.com.br>
2025-08-29 12:46:53 -0300

    ajuste cors

---
+++ config/cors.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    'paths' => ['api/*', 'login', 'logout', 'me', 'logout'],
+
+    'allowed_methods' => ['*'],
+
+    'allowed_origins' => [
+        'https://gerenciado-app.netlify.app',
+    ],
+
+    'allowed_origins_patterns' => [],
+
+    'allowed_headers' => ['*'],
+
+    'exposed_headers' => [],
+
+    'max_age' => 0,
+
+    'supports_credentials' => true,
+];
