c14cf626f74a17ec56aea97fa1a8d3d1552fd7f1
Gabriel Rhoden <gabrielrhodden@gmail.com>
2025-08-30 11:13:40 -0300

    ajuste login

--- app/Models/User.php
+++ app/Models/User.php
@@ -30,14 +30,12 @@ class User extends Authenticatable implements JWTSubject
         'remember_token',
     ];
 
+    protected $casts = [
+    'email_verified_at' => 'datetime',
+    'password' => 'hashed',
+    ];
 
-    protected function casts(): array
-    {
-        return [
-            'email_verified_at' => 'datetime',
-            'password' => 'hashed',
-        ];
-    }
+
 
     public function getJWTIdentifier()
     {
